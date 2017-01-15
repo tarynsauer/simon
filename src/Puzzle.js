64bd0aeb8103482cedfdbdcd6d80057b7d2dbf36
Taryn Sauer <taryn.sauer@gmail.com>
2017-01-15 15:10:54 -0600

    wip run puzzle seq

--- src/Puzzle.js
+++ src/Puzzle.js
@@ -20,16 +20,40 @@ function toggle(elem, className) {
   elem.classList.toggle(className);
 }
 
+function selectPuzzleSquares(puzzle) {
+  if(puzzle.length != 0) {
+    const id = puzzle[0];
+    const elem = document.querySelector("[data-id='" + `${id}` + "']");
+    toggle(elem, "bright");
+    sleep(750).then(() => {
+      toggle(elem, "bright");
+      puzzle.shift()
+      selectPuzzleSquares(puzzle);
+    });
+  }
+}
+
+function runSequence() {
+  const puzzle = generatePuzzle(4);
+  selectPuzzleSquares(puzzle);
+}
+
 function selectSquare(event) {
   const elem = event.target;
   toggle(elem, "bright");
-  sleep(250).then(() => {
+  sleep(750).then(() => {
     toggle(elem, "bright");
     guesses.push(elem.dataset.id);
   });
 }
 
 const squares = document.getElementsByClassName("square");
+const start = document.getElementById("start");
+
 for (var square of squares) {
   square.addEventListener("click", selectSquare, false);
 }
+
+start.addEventListener("click", runSequence, false);
+
+
